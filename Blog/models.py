a7323afb51869e49f6775f075cab7a8c29c0083b
Hasfa123 <hasfa04@gmail.com>
2019-01-24 23:43:36 +0300

    Added provision for adding comments

--- Blog/models.py
+++ Blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 # Create your models here.
 
@@ -17,5 +18,22 @@ class Post(models.Model):
 		self.published_date = timezone.now()
 		self.save()
 
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
+
 	def __str__(self):
 		return self.title
+
+class Comment(models.Model):
+	post = models.ForeignKey('Blog.Post', on_delete=models.CASCADE, related_name='comments')
+	author = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
+
+	def approve(self):
+		self.approved_comment = True
+		self.save()
+
+	def __str__(self):
+		return self.text
